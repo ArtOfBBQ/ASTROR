59a0ce68ee4c741ecdf0159d8ad84d285ee1658a
Jelle Van den Eynde <40927974+ArtOfBBQ@users.noreply.github.com>
2020-04-24 05:19:31 +0900

    Rotating objects

--- helloworld.js
+++ helloworld.js
@@ -1,20 +1,29 @@
 // Apparently we can listen for keys without involving HTML
+let iPlayerControlled = 1;
 pagebody.addEventListener('keydown', function (e) {
     switch (e.key) 
     {
+        case  'ArrowUp':
+            allGameObjects[iPlayerControlled].xVelocity += Math.sin(allGameObjects[iPlayerControlled].angle) * 0.2;
+            if (allGameObjects[iPlayerControlled].xVelocity > 1) {allGameObjects[iPlayerControlled].xVelocity = 1};
+            allGameObjects[iPlayerControlled].yVelocity += -Math.cos(allGameObjects[iPlayerControlled].angle) * 0.2;
+            if (allGameObjects[iPlayerControlled].yVelocity > 1) {allGameObjects[iPlayerControlled].yVelocity = 1};
+            break;
+        case  'ArrowDown':
+            console.log(Math.sin(allGameObjects[iPlayerControlled]).angle);
+            // allAsteroids[1].yVelocity += sin(allAsteroids.angle) * 1;
+            break;
         case  'ArrowLeft':
-            allAsteroids[1].xVelocity -= 1;
-            screenHandler.Render();
+            allGameObjects[iPlayerControlled].angle -= 0.1;
             break;
         case 'ArrowRight':
-            allAsteroids[1].xVelocity += 1;
-            screenHandler.Render();
+            allGameObjects[iPlayerControlled].angle += 0.1;
             break;
     }
 }, true);
 
 // A 2D array of 'pixels' (actually text characters)
-// that we're using a toy screen
+// that we're using as a toy screen
 class Screen {
     constructor(width, height) 
     {
@@ -50,9 +59,9 @@ class Screen {
     {
         let htmlScreenContent = '';
         
-        for (let i = 0; i < this.width; i++) 
+        for (let y = 0; y < this.height; y++)
         {
-            for (let y = 0; y < this.height; y++)
+            for (let i = 0; i < this.width; i++) 
             {
                 if (this.screenContent[i][y] == true) 
                 {
@@ -74,27 +83,32 @@ class MoveableObject
 {
     constructor(xPos, yPos) 
     {
+        // xPos and yPos are the coordinates of this object on the 2-D map
         this.xPos = xPos;
         this.yPos = yPos;
+        // xVelocity is the rate at which this object is moving along the x-axis
         this.xVelocity = 0;
         this.yVelocity = 0;
-        this.size = 6;
+        // the angle describes the rotation of this object
+        this.angle = 0.9;
+        // The polygon attribute is a collection of connectable dots
+        // representing this object when it faces directly upwards
+        this.polygon = [[0, -3], [-3, +3], [+3, +3]]
     }
 
     drawToScreen()
     {
-        for (let i = 0; i < this.size; i++) 
+        for (let i = 0; i < this.polygon.length; i++) 
         {
-            for (let y = 0; y < this.size; y++) 
-            {
-                console.log(typeof screenHandler);
-                console.log(typeof screenHandler.screenContent);
-                console.log('i:' + this.xPos + i + ' ' + screenHandler.screenContent.length);
-                console.log(typeof screenHandler.screenContent[this.xPos + i]);
-                console.log('y:' + this.yPos + y + ' ' + screenHandler.screenContent[0].length);
-                console.log(typeof screenHandler.screenContent[Math.round(this.xPos + i)][Math.round(this.yPos + y)]);
-                screenHandler.screenContent[Math.round(this.xPos + i)][Math.round(this.yPos + y)] = true;
-            }
+            // rotation of this point
+            let rotatedxpos = this.polygon[i][0] * Math.cos(this.angle) - (this.polygon[i][1] * Math.sin(this.angle));
+            let rotatedypos = this.polygon[i][0] * Math.sin(this.angle) + (this.polygon[i][1] * Math.cos(this.angle));
+            
+            // movement of this point
+            let newxpos = Math.round(this.xPos + rotatedxpos);
+            let newypos = Math.round(this.yPos + rotatedypos);
+            
+            screenHandler.screenContent[newxpos][newypos] = true;
         }
     }
 }
@@ -104,19 +118,35 @@ function updateSimulation()
 {
     elapsedTime = 1;
 
-    // update and draw asteroids
-    for (let i = 0; i < allAsteroids.length; i++)
+    // draw a border around the screen
+    for (let i = 0; i < screenHandler.width; i++) 
     {
-        allAsteroids[i].xPos += allAsteroids[i].xVelocity * elapsedTime;
-        allAsteroids[i].yPos += allAsteroids[i].yVelocity * elapsedTime;
-        allAsteroids[i].drawToScreen();
+        screenHandler.screenContent[i][0] = true;
+        screenHandler.screenContent[i][screenHandler.height - 1] = true;
+    }
+
+    for (let i = 0; i < screenHandler.height; i++) 
+    {
+        screenHandler.screenContent[0][i] = true;
+        screenHandler.screenContent[screenHandler.width - 1][i] = true;
+    }
+
+    // rotate objects
+
+
+    // update and draw asteroids
+    for (let i = 0; i < allGameObjects.length; i++)
+    {
+        allGameObjects[i].xPos += allGameObjects[i].xVelocity * elapsedTime;
+        allGameObjects[i].yPos += allGameObjects[i].yVelocity * elapsedTime;
+        allGameObjects[i].drawToScreen();
     }
 }
 
 function gameLoop(timestamp) {
 
     elapsedTime = timestamp - lastRender;
-    console.log(allAsteroids[0].xPos);
+    console.log(allGameObjects[0].xPos);
     
     screenHandler.Clear();
     updateSimulation();
@@ -129,8 +159,7 @@ function gameLoop(timestamp) {
 
 let elapsedTime = 0;
 let lastRender = 0;
-screenHandler = new Screen(400, 600);
-let allAsteroids = [new MoveableObject(4, 4), new MoveableObject(100, 20)];
-allAsteroids[0].xVelocity = 0.05;
+screenHandler = new Screen(600, 300);
+let allGameObjects = [new MoveableObject(4, 4), new MoveableObject(50, 20)];
 
 gameLoop();
